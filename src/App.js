b42f1c29963e39f5adcff5e24f1b880db7176aeb
Ektiar <eng.ektiar@gmail.com>
2022-03-30 22:02:48 +0600

    Array map Good work

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import Card from './components/body/Card';
+import Header from './components/header/Header';
+
+
+
+function App(){
+return   <div>
+     <Header />
+    <Card />
+</div>
+}
+
+export default App;
---
+++ src/components/body/Card.js
@@ -0,0 +1,41 @@
+import avater from "../../../src/image/avatar.png";
+import "./style.css";
+import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import Date from "../../data.json";
+
+function Card() {
+  return (
+    <Container>
+      <Row>
+        {Date.map((item) => (
+          <Col md={4}>
+            <div class="cards">
+              <div class="cards-body">
+                <img src={avater} alt="Logo" />
+                <p class="cards-title">{item.name}</p>
+                <p class="cards-title">{item.designation}</p>
+                <p class="cards-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <Button variant="primary">Secondary</Button>
+              </div>
+            </div>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+}
+
+//     <div class="wrapper">
+//         <div class="card">
+//          <div class="card-body">
+//          <img src={avater} alt="Logo"/>
+//          <p class="card-title">{name}</p>
+//          <p class="card-title">{designation}</p>
+//         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//     <button class="btn btn-success">Add</button>
+//   </div>
+// </div>
+//         </div>
+
+export default Card;
